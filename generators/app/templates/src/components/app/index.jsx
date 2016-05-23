0b1be96528af96a683fc9a13724a66d29530d660
Vilvarin <viilvariin@gmail.com>
2016-05-23 15:11:51 +0500

    add routing

--- generators/app/templates/src/components/app/index.jsx
+++ generators/app/templates/src/components/app/index.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div id="main">
+      <div id="content">
         <h1>It's work!</h1>
         <hr />
         {this.props.children}
